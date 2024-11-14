d23771e273a5a79f20296180dd82f4ef7a03fe38
rafkrnwnworkspace <136820310+rafkrnwnworkspace@users.noreply.github.com>
2024-11-14 22:45:26 +0800

    Membuat JFrame baru

--- src/CekCuacaFrame.java
+++ src/CekCuacaFrame.java
@@ -7,12 +7,12 @@
  *
  * @author rafkr
  */
-public class CekCuaca extends javax.swing.JFrame {
+public class CekCuacaFrame extends javax.swing.JFrame {
 
     /**
-     * Creates new form CekCuaca
+     * Creates new form CekCuacaFrame
      */
-    public CekCuaca() {
+    public CekCuacaFrame() {
         initComponents();
     }
 
@@ -58,20 +58,20 @@ public class CekCuaca extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CekCuaca.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CekCuacaFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CekCuaca.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CekCuacaFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CekCuaca.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CekCuacaFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CekCuaca.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CekCuacaFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new CekCuaca().setVisible(true);
+                new CekCuacaFrame().setVisible(true);
             }
         });
     }
